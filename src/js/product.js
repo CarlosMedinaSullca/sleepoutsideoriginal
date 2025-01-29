bfc4ad58379e94ac6a5a72f0ee3f10722feb0f39
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2025-01-29 15:52:02 -0400

    week 3 updates

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,11 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParams("product");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productID, dataSource);
+product.init();
+loadHeaderFooter();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,12 +7,35 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key))|| [];
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
+  updateCartCount()
 }
+
+export function getParams(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+ ) {
+  const strings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, strings.join(""));
+ }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -20,4 +43,46 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+// cart count
+
+export function updateCartCount(){
+  const cartCount = document.querySelector(".cart-count")
+  cartCount.innerHTML = ""
+  const count = getLocalStorage("so-cart").length
+  if (count>0){
+    cartCount.innerHTML = count
+  } else {
+    cartCount.innerHTML = 0
+  }
+}
+
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  callback
+) {
+  parentElement.insertAdjacentHTML("afterbegin",template);
+  if (callback) {
+    callback()
+  }
+}
+
+export async function loadHeaderFooter(){
+  const headerElement = document.querySelector("#main-header")
+  const headerTemplate = await loadTemplate("../partials/header.html")
+
+  const footerElement = document.querySelector("#main-footer")
+  const footerTemplate = await loadTemplate("../partials/footer.html")
+  
+  renderWithTemplate(headerTemplate,headerElement,null,updateCartCount)
+  renderWithTemplate(footerTemplate,footerElement)
+}
+
+export async function loadTemplate(path){
+  const html = await fetch(path)
+  const template = await html.text()
+  return template
 }
